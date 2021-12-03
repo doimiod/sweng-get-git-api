7778216493b09ddfc9fce7a9ae076663ec141e4b
doimiod <doim@tcd.ie>
2021-12-03 18:39:58 +0000

    some changes

--- getGitApi.py
+++ getGitApi.py
@@ -6,7 +6,7 @@ def main():
     usr = g.get_user()
     print("user: " + usr.login)
     print("fullname: " + usr.name)
-    #print("location: " + usr.login)
-    # #print("company: " + use.name)
+    print("location: " + usr.location)
+    print("company: " + usr.company)
 
 main()
